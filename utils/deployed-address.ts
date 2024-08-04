18baa62aea149abb1a4e09af19a091be547f47a7
shitouwuhao <shitouwuhao@gmail.com>
2024-08-04 06:30:12 +0000

    t6

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x4467895c8a9B4272c87Ac03f6D050b49BA565533'
+export const ProxyAdmin = '0xF2D82B8EB23723cB1f0B0A6B4cAa609e1B34F840'
+export const SWTRImplementation = '0x95bf8d51404BcCB391c9ed9239755300F2d9466b'
     
